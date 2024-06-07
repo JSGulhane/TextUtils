3a3f173e5652087f3bfe4909bc07d61e3a5db508
JSGulhane <jinalsgulhane@gmail.com>
2024-06-08 02:43:42 +0530

    dark/light modes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
    <>
-    <Navbar title= "TextUtils" aboutText= "About"/>
+    <Navbar title= "TextUtils"/>
     <div className='container my-3'>
-    <TextForm heading= "Enter the text to analyse below:"/>
+    {/* <TextForm heading= "Enter the text to analyse below:"/> */}
+    <About/>
     </div>
     
    </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,24 +9,60 @@ export default function TextForm(props) {
             setText(newText);
         }
 
+    const handleLowClick = () =>
+        {
+            let newText = text.toLowerCase();
+            setText(newText);
+            }
+
+     const handleClearClick = () =>
+        {
+            setText('');
+        }
+    
+    const handleCopyClick = () =>
+        {
+            navigator.clipboard.writeText(text);
+            alert('Text copied to clipboard');
+        }
+
+   
+
     const handleOnChange = (event)=>
         {
           //  console.log('handle on change');
             setText(event.target.value);
         }
   
-    const [text, setText] = useState('Enter text here please');  //text = "helloo"; wrong way  //setText("hellooo"); correct way to update text
+    const [text, setText] = useState('');  //text = "helloo"; wrong way  //setText("hellooo"); correct way to update text
  
 
   return (
-    <div> 
+    <>
+    <div className="container"> 
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
 
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
+    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+    <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
+
 
     </div>
+
+    <div className="container my-3">
+        <h2>Your Text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+
+
+
+    </>
   )
 }
