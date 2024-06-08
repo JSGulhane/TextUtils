e1fcce8be623f5d23942e978c217e086600a74a6
JSGulhane <jinalsgulhane@gmail.com>
2024-06-09 03:07:14 +0530

    Router setup -home, about

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './components/About';
 import Alert from './components/Alert';
 import React, { useState } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
@@ -20,30 +22,73 @@ function App() {
       }, 1500);
     }
 
-  const toggleMode = ()=> {
+  const toggleMode1 = ()=> {
     if(mode === 'light')
       {
         setMode('dark')
         document.body.style.backgroundColor = '#042743';
-        showAlert("Dark mode enabled","success");
+        showAlert("Blue mode enabled","success");
+        document.title = 'TextUtils - Blue Mode';
       }
       else{
         setMode('light') 
         document.body.style.backgroundColor = 'white';
         showAlert("Light mode enabled","success");
+        document.title = 'TextUtils - Light Mode';
 
+      }
+  }
+
+  const toggleMode2 = ()=> {
+    if(mode === 'light')
+      {
+        setMode('dark')
+        document.body.style.backgroundColor = '#8B0000';
+        showAlert("Red mode enabled","success");
+        document.title = 'TextUtils - Red Mode';
+
+      }
+      else{
+        setMode('light') 
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode enabled","success");
+        document.title = 'TextUtils - Light Mode';
+
+      }
+  }
+  const toggleMode3 = ()=> {
+    if(mode === 'light')
+      {
+        setMode('dark')
+        document.body.style.backgroundColor = '#013220';
+        showAlert("Green mode enabled","success");
+        document.title = 'TextUtils - Green Mode';
+      }
+      else{
+        setMode('light') 
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode enabled","success");
+        document.title = 'TextUtils - Light Mode';
 
       }
   }
 
   return (
    <>
-    <Navbar title= "TextUtils" mode= {mode} toggleMode = {toggleMode}/>
+    <Router>
+    <Navbar title= "TextUtils" mode= {mode} toggleMode1 = {toggleMode1} toggleMode2 = {toggleMode2} toggleMode3 = {toggleMode3}/>
     <Alert alert = {alert}/>
     <div className='container my-3'>
-    <TextForm showAlert= {showAlert} heading= "Enter the text to analyse below:" mode = {mode}/>
-    {/* <About/> */}
+ 
+      <Routes>
+        {/* exact - use exact to get to the exact location , react does partial matching, meaning if we have two component
+        say on    /users (c1)    and   /users/home (c2)   so react will check the whole path if exact is written, doesnt change anything in this project*/}
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/" 
+        element={ <TextForm showAlert= {showAlert} heading= "Enter the text to analyse below:" mode = {mode}/> } />
+      </Routes>
     </div>
+    </Router>
     
    </>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -69,7 +69,7 @@ export default function TextForm(props) {
 
     <div className="container my-3"  style={{color: props.mode==='dark' ? 'white' : 'black'}}>
         <h2>Your Text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").length - 1 + " "} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0 ?text:"Enter something in the textbox to Preview it"}</p>
